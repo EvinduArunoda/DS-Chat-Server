c129fc287b765eba24b610251a500ad37f5e5f16
EvinduArunoda <earajapaksha.17@cse.mrt.ac.lk>
2021-08-27 12:32:11 +0530

    ts configured

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 const express = require('express');
+import {test} from './utils';
 
 const app = express();
 
-const port = process.env.PORT || 4041 ;
+const port = process.env.PORT || 4042 ;
 
-app.get('/', (_,res) => {
+app.get('/', (_:any,res:any) => {
     res.send('Your Express App');
 });
 
 
 app.listen(port, () => {
+    test();
     console.log(`Server is running on port: ${port}`)
 });
