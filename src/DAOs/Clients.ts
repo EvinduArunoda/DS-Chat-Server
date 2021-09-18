03f6ae7229551d7a6b1491fa666d4cfdaa27b7d1
Udara Pathum <hwupathum@gmail.com>
2021-09-19 00:02:03 +0530

    identity validation

--- src/DAOs/Clients.ts
+++ src/DAOs/Clients.ts
@@ -3,15 +3,15 @@ import {Socket} from "net";
 
 export const clients: ClientInterface = {};
 
-export const IsRegistered = (identity: string): boolean => {
+export const isRegistered = (identity: string): boolean => {
     return Object.keys(clients).includes(identity);
 }
 
-export const AddNewClient = (identity: string, sock: Socket): void => {
+export const addNewClient = (identity: string, sock: Socket): void => {
     clients[identity] = sock;
 }
 
-export const RemoveClient = (sock: Socket): boolean => {
+export const removeClient = (sock: Socket): boolean => {
     const idx = Object.values(clients).indexOf(sock);
     if (idx == -1) return false;
     const identity = Object.keys(clients)[idx]
--- src/Services/ClientSrvice/clientService.ts
+++ src/Services/ClientSrvice/clientService.ts
@@ -1,25 +1,24 @@
 import {Socket} from "net";
-import {writeJSONtoSocket} from "../../Utils/utils";
-import {AddNewClient, IsRegistered, RemoveClient} from "../../DAOs/Clients";
+import {writeJSONtoSocket, isValidIdentity} from "../../Utils/utils";
+import {addNewClient, isRegistered, removeClient} from "../../DAOs/Clients";
 
 export class ClientService {
     constructor() {}
 
     registerClient(data: any, sock: Socket): boolean {
         const identity = data.identity;
-
-        if (IsRegistered(identity)) {
+        if (!isValidIdentity(identity) || isRegistered(identity)) {
             writeJSONtoSocket(sock, {type: "newidentity", approved: "false"});
             return false
         } else {
             // Check id in other servers
-            AddNewClient(identity, sock);
+            addNewClient(identity, sock);
             writeJSONtoSocket(sock, {type: "newidentity", approved: "true"});
             return true
         }
     }
 
     removeClient(sock: Socket): boolean {
-        return RemoveClient(sock);
+        return removeClient(sock);
     }
 }
