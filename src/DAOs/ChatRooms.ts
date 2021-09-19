c16457b5f10a8b1b586b5473a2713b87fa707167
EvinduArunoda <earajapaksha.17@cse.mrt.ac.lk>
2021-09-19 21:26:00 +0530

    dao rooms

--- src/DAOs/ChatRooms.ts
+++ src/DAOs/ChatRooms.ts
@@ -1,8 +1,16 @@
 import {ChatRoomInterface} from "../Interfaces/ChatRoomInterface";
+import {clients} from "./Clients";
 
 export const chatRooms: ChatRoomInterface = {};
 
 export const addNewChatRoom = (roomId: string, owner: string): void => {
-    chatRooms[roomId] = {owner : owner, participants: []}
+    chatRooms[roomId] = {owner : owner, participants: [owner]}
 }
 
+export const addParticipant = (roomId: string, participant: string): void => {
+    chatRooms[roomId].participants = [...chatRooms[roomId].participants, participant]
+}
+
+export const removeParticipant = (roomId: string, participant: string): void => {
+
+}
