a072c23cea97874b18605f468e0ad2f59c8ecf3d
Udara Pathum <hwupathum@gmail.com>
2021-09-19 16:04:01 +0530

    isvalid entry into clients

--- src/DAOs/Clients.ts
+++ src/DAOs/Clients.ts
@@ -1,10 +1,17 @@
-import {ClientInterface} from "../Interfaces/clientInterface";
+import {ClientInterface} from "../Interfaces/ClientInterface";
 import {Socket} from "net";
 
 export const clients: ClientInterface = {};
 
 export const isRegistered = (identity: string): boolean => {
+    if (!isValidIdentity) return false;
     return Object.keys(clients).includes(identity);
+}
+
+export function isValidIdentity(identity: string): boolean {
+    // check if identity is alphanumeric string starting with an upper or lower case character. 
+    // It must be at least 3 characters and no more than 16 characters long.
+    return /^[A-Za-z]{1}[A-Za-z0-9]{2,15}$/.test(identity)
 }
 
 export const addNewClient = (identity: string, sock: Socket): void => {
