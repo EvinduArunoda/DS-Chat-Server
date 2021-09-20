da14a3c3d6675ad54838381963e52558e496e028
EvinduArunoda <earajapaksha.17@cse.mrt.ac.lk>
2021-09-20 15:13:00 +0530

    dao,service locator added

--- src/DAOs/Clients.ts
+++ src/DAOs/Clients.ts
@@ -1,6 +1,6 @@
-import {ClientInterface} from "../Interfaces/ClientInterface";
 import {Socket} from "net";
 import { isValidIdentity } from "../Utils/utils";
+import {ClientInterface} from "../Interfaces/clientInterface";
 
 export const clients: ClientInterface = {};
 
@@ -18,4 +18,27 @@ export const removeClient = (sock: Socket): boolean => {
     if (idx == -1) return false;
     const identity = Object.keys(clients)[idx]
     return delete clients[identity];
+}
+
+// Clients DAO
+export class ClientsDAO {
+    constructor(
+    ) {
+    }
+
+    isRegistered (identity: string): boolean {
+        if (!isValidIdentity) return false;
+        return Object.keys(clients).includes(identity);
+    }
+
+    addNewClient(identity: string, sock: Socket): void {
+        clients[identity] = sock;
+    }
+
+    removeClient(sock: Socket): boolean {
+        const idx = Object.values(clients).indexOf(sock);
+        if (idx == -1) return false;
+        const identity = Object.keys(clients)[idx]
+        return delete clients[identity];
+    }
 }
---
+++ src/Utils/serviceLocator.ts
@@ -0,0 +1,17 @@
+import {ClientsDAO} from "../DAOs/Clients";
+
+export class ServiceLocator {
+    private static readonly _instances: Map<String, any> = new Map<String, any>();
+
+    private constructor() {
+    }
+
+    static get clientsDAO(): ClientsDAO {
+        const key = 'clientsDAO';
+        if (!this._instances.get(key)) {
+            this._instances.set(key, new ClientsDAO());
+        }
+        return this._instances.get(key);
+    }
+
+}
