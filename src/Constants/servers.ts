fec1020c167186f6d9088270f4f969751bafd309
Bashana Elikewela <hbashanae@gmail.com>
2021-10-16 10:16:43 +0530

    get higher ids

--- src/Constants/servers.ts
+++ src/Constants/servers.ts
@@ -1,11 +1,12 @@
 import _ from "lodash";
 import { Server, ServerInterface } from "../Interfaces/ServerInterface"
 
-
+// TODO: Read from config file
 export class ServerList {
     private serverList: ServerInterface = {
-        s1: { host: 'localhost', port: 4444 },
-        s2: { host: 'localhost', port: 4445 }
+        1: { host: 'localhost', port: 4444 },
+        2: { host: 'localhost', port: 4445 },
+        3: { host: 'localhost', port: 4446 }
     }
 
     getServerIds(): string[] {
@@ -14,6 +15,16 @@ export class ServerList {
 
     getServer(serverid: string): Server {
         return this.serverList[serverid];
+    }
+
+    getHigherUpServers(): Server[]{
+        const higherUpServers = []
+        for (let key in this.serverList){
+            if(key < (process.env.SERVER_ID as string)){
+                higherUpServers.push(this.serverList[key])
+            }
+        }
+        return higherUpServers
     }
 
 }
