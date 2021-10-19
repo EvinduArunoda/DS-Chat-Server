32683e690a5f79d86bfa2a99ba45d085d733f553
Bashana Elikewela <hbashanae@gmail.com>
2021-10-19 19:13:21 +0530

    change startElection return type

--- src/Services/electionService.ts
+++ src/Services/electionService.ts
@@ -15,19 +15,20 @@ export class ElectionService {
 
     static async approveElection(data: any, sock: Socket): Promise<boolean> {
         const {serverid} = data
-        console.log('approveElection', serverid, getServerId())
         if(parseInt(serverid) > parseInt(getServerId())){
             writeJSONtoSocket(sock, { type: "startelection", serverid, approved: false })
             ElectionService.startElection()
-            // ServiceLocator.leaderDAO.setLeaderId(await ElectionService.startElection())
         }
         return true
     }
 
     constructor() { }
 
-    static async startElection(): Promise<string> {
-        console.log('START ELECTION')
+    static async startElection(): Promise<void> {
+        // Ask higher id servers, wait T0
+        // if get bullied, wait T1. If before the timeout, leader is not elected restart election
+        // else, elect itsef as the leader and broadcast every server
+        // set leaderId
         const T0 = 30000
         const T1 = 20000
         // set leaderid as emptystring
@@ -35,14 +36,11 @@ export class ElectionService {
 
         return new Promise((resolve, reject) => {
             const higherUpServers = new ServerList().getHigherUpServers()
-            console.log('HIGH SERVERS', higherUpServers);
 
             if(higherUpServers.length < 1){
                 const leaderId = getServerId()
-                console.log('NO HIGHER SERVERS LEADER',getServerId())
                 ServiceLocator.leaderDAO.setLeaderId(leaderId)
                 LeaderService.broadcastServers({type: "declareleader", serverid: leaderId})
-                resolve(leaderId)
             }
 
             //hardcoded timeout
@@ -59,10 +57,7 @@ export class ElectionService {
                         //this time out should be larger than socket timeout; otherwise a election is called before completing a election
                         setTimeout(() => {
                             if (ServiceLocator.leaderDAO.getLeaderId()) {
-                                resolve(ServiceLocator.leaderDAO.getLeaderId())
-                                console.log("(timeout msg) ON DATA LEADER HAS SELECTED",ServiceLocator.leaderDAO.getLeaderId(), port)
                             } else {
-                                console.log('timeout: Again election')
                                 ElectionService.startElection()
                             }
                         }, 60000)
@@ -70,15 +65,12 @@ export class ElectionService {
                 })
 
                 socket.on('timeout', () => {
-                    console.log('socket timeOUT', host, port);
                     setTimeout(()=>{
                         if (!ServiceLocator.leaderDAO.getLeaderId()) {
-                            console.log('socket timeout NO LEADER');
                             // choose itself as the leader
                             const leaderId = getServerId()
                             ServiceLocator.leaderDAO.setLeaderId(leaderId)
                             LeaderService.broadcastServers({type: "declareleader", serverid: leaderId})
-                            resolve(leaderId)
                         }
                         socket.end();
                     },higherUpServers.length == 1 ? 200: 10000*parseInt(getServerId()))
@@ -86,27 +78,18 @@ export class ElectionService {
                 });
 
                 socket.on('error', () => {
-                    console.log('socket error election service', port)
                     setTimeout(() => {
                         if (ServiceLocator.leaderDAO.getLeaderId()) {
-                            resolve(ServiceLocator.leaderDAO.getLeaderId())
                             console.log('A leader has been selected',ServiceLocator.leaderDAO.getLeaderId())
                         } else {
-                            console.log('SOCKET ERROR no leader')
                             const leaderId = getServerId()
                             ServiceLocator.leaderDAO.setLeaderId(leaderId)
                             LeaderService.broadcastServers({type: "declareleader", serverid: leaderId})
-                            resolve(leaderId)
                         }
                         socket.end();
                     }, higherUpServers.length == 1 ? 200: T1 + 2000*parseInt(getServerId()))
                 });
             })
         })
-
-        // TODO: Ask higher id servers, wait T0
-        // if get bullied, wait T1. If before the timeout, leader is not elected restart election
-        // else, elect itsef as the leader and broadcast every server
-        // return leaderId
     }
 }
