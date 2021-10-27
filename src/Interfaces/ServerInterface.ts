8225a113f06fc9769c09610cc121a4ffd148e119
Udara <hwupathum@gmail.com>
2021-10-27 19:35:10 +0530

    for submission

--- src/Interfaces/ServerInterface.ts
+++ src/Interfaces/ServerInterface.ts
@@ -18,7 +18,6 @@ export class Server {
     }
 
     get serverAddress(): string {
-        if((this.serverid + getServerIdNumber()) % 2 === 0) return 'localhost'
-        return this.address
+        return `s${this.serverid}`
     }
 }
